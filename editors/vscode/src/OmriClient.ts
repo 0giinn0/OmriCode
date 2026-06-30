e8751d9538f4e9eda06b2385e51bf58b24c34b19
0giinn0 <omerchaudhry760@gmail.com>
2026-06-30 17:29:35 +0200

    Fix CI: disable no-constant-condition eslint rule in OmriClient.ts

--- editors/vscode/src/OmriClient.ts
+++ editors/vscode/src/OmriClient.ts
@@ -94,6 +94,7 @@ export class OmriClient {
     let buffer = '';
 
     try {
+      // eslint-disable-next-line no-constant-condition
       while (true) {
         const { done, value } = await reader.read();
         if (done) break;
